672b11e5009aef61fbc3fb11a1a4ac68c2f01dd8
zimin <jianming.zeng@directdrivetech.com>
2025-08-27 18:16:02 +0800

    Imporve work flow
    Signed-off-by: zimin <jianming.zeng@directdrivetech.com>

---
+++ generated/version.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Auto-generated version information from Git and CMake Presets
+#define MAJOR_VERSION 0
+#define MINOR_VERSION 0
+#define PATCH_VERSION 0
+#define TWEAK_VERSION 2
+#define HASH_VERSION  "04492bd"
+
+#define FULL_VERSION "0.0.0.2.04492bd"
---
+++ include/can_ipc_layer/can_ipc_layer.h
@@ -0,0 +1,30 @@
+// Copyright (c) Direct Drive Technology Co., Ltd. All rights reserved.
+// Author: Zi Min <jianming.zeng@directdrivetech.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __LIBCAN_IPC_LAYER_H__
+#define __LIBCAN_IPC_LAYER_H__
+
+#ifdef __cplusplus
+extern "C"{
+#endif // __cplusplus
+
+
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __LIBCAN_IPC_LAYER_H__
